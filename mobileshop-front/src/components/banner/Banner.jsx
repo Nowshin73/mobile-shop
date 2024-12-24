8989e5bc0e0592af9aea3356a147548a92d354d1
Nowshin73 <68994761+Nowshin73@users.noreply.github.com>
2024-12-24 14:03:52 +0600

    banner and product details updated

--- mobileshop-front/src/components/banner/Banner.jsx
+++ mobileshop-front/src/components/banner/Banner.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const Banner = () => {
     return (
         <div style={{
-            backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://i.ibb.co/mTGV9VR/image.png")',
+            backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://i.ibb.co.com/nCGNYzq/low-poly-abstract-banner-design-2708.jpg")',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
           }} className="hero min-h-screen">
             <div className="hero-content text-center">
+                <div><img src="https://i.ibb.co.com/VVdK6FG/image.png" alt="" /></div>
                 <div className="w-2/3">
                     <h1 style={{textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)"}} className="text-5xl text-white font-bold">Your Gateway to the Latest Smartphones & Accessories!</h1>
                     <p className="py-6 text-white ">
--- mobileshop-front/src/pages/Products/ProductDetails.jsx
+++ mobileshop-front/src/pages/Products/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import ReactStars from "react-rating-stars-component"
 // Import Swiper styles
 import 'swiper/css';
@@ -15,59 +15,59 @@ import Loader from '../../components/loader/Loader';
 import Reviewcard from './Reviewcard';
 
 const ProductDetails = () => {
-  const { id } = useParams();
+  const product = useLoaderData();
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  //   const product = {       
+  //     name: "Matrix Tennis necklace1",
+  //     description: "Delivering a modern take on a truly classic style, this Matrix Tennis necklace is distinguished by the purity and elegance of its shimmering clear stones and rhodium plated design. Each individual stone works beautifully with the next, creating an endless stream of glamour that is perfect for your everyday look. Enjoy it for yourself, or as a gift for someone special.",
+  //     price: 500,
+  //     category: "jwelry",
+  //     numofReviews: 4,
+  //     ratings: 3.6,
+  //     images: [{ public_id:"img1",
+  //             url:"https://i.ibb.co.com/10bzw9k/image.png"
+  //             },
+  //             { public_id:"img2",
+  //             url:"https://i.ibb.co.com/wWchmCL/image.png"
+  //             },
+  //             { public_id:"img3",
+  //             url:"https://i.ibb.co.com/wLhZ16V/image.png"
+  //             },
+  //             { public_id:"img4",
+  //               url:"https://i.ibb.co.com/C7qtmDs/image.png"
+  //             }],
+  //     reviews: [
+  //          {
+  //             name:"Nowshin",
+  //             rating: 4.5,
+  //             comment:" this is a comment"
+  //         },
+  //         {
+  //             name:"Nawar",
+  //             rating: 3,
+  //             comment:" this is 2nd comment"
+  //         },
+  //         {
+  //             name:"Nafisa",
+  //             rating: 2,
+  //             comment:" this is 3nd comment"
+  //         },
+  //         {
+  //             name:"Nishat",
+  //             rating: 5,
+  //             comment:" this is 4th comment"
+  //         }
+  //     ]
+  // };
 
-  const product = {       
-    name: "Matrix Tennis necklace1",
-    description: "Delivering a modern take on a truly classic style, this Matrix Tennis necklace is distinguished by the purity and elegance of its shimmering clear stones and rhodium plated design. Each individual stone works beautifully with the next, creating an endless stream of glamour that is perfect for your everyday look. Enjoy it for yourself, or as a gift for someone special.",
-    price: 500,
-    category: "jwelry",
-    numofReviews: 4,
-    ratings: 3.6,
-    images: [{ public_id:"img1",
-            url:"https://i.ibb.co.com/10bzw9k/image.png"
-            },
-            { public_id:"img2",
-            url:"https://i.ibb.co.com/wWchmCL/image.png"
-            },
-            { public_id:"img3",
-            url:"https://i.ibb.co.com/wLhZ16V/image.png"
-            },
-            { public_id:"img4",
-              url:"https://i.ibb.co.com/C7qtmDs/image.png"
-            }],
-    reviews: [
-         {
-            name:"Nowshin",
-            rating: 4.5,
-            comment:" this is a comment"
-        },
-        {
-            name:"Nawar",
-            rating: 3,
-            comment:" this is 2nd comment"
-        },
-        {
-            name:"Nafisa",
-            rating: 2,
-            comment:" this is 3nd comment"
-        },
-        {
-            name:"Nishat",
-            rating: 5,
-            comment:" this is 4th comment"
-        }
-    ]
-};
-
-const options = {
-  edit: false,
-  color: "rgba(20,20,20,0.5)",
-  activeColor: "orange",
-  size: window.innerWidth < 600 ? 20 : 25,
-  value: product.ratings,
-  isHalf: true
-}
+  const options = {
+    edit: false,
+    color: "rgba(20,20,20,0.5)",
+    activeColor: "orange",
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: product ? product.rating?.rating : 3.5,
+    isHalf: true
+  }
 
   return (
     <Fragment>
@@ -88,16 +88,19 @@ const options = {
                   modules={[FreeMode, Navigation, Thumbs]}
                   className="mySwiper2 w-[300px] lg:w-[500px]"
                 >
-                  {product.images &&
+                  {/* {product.images &&
                     product.images.map((pic, index) => (
                       <SwiperSlide key={index}>
                         <img src={pic.url} alt={`product-image-${index}`} className="" />
                       </SwiperSlide>
-                    ))}
+                    ))} */}
+                  <SwiperSlide>
+                    <img src={product.images} alt={`product-image`} className="" />
+                  </SwiperSlide>
                 </Swiper>
 
                 {/* Thumbnail Swiper */}
-                {product.images && (
+                {/* {product.images && (
                   <Swiper
                     onSwiper={setThumbsSwiper}
                     spaceBetween={10}
@@ -113,7 +116,7 @@ const options = {
                       </SwiperSlide>
                     ))}
                   </Swiper>
-                )}
+                )} */}
               </div>
             </div>
 
@@ -134,12 +137,12 @@ const options = {
                     ))}
                   </ul> */}
               <div className="add-cart flex items-center gap-5">
-                <div className="quantity">
+                {/* <div className="quantity">
                   <button className='px-4 text-2xl border bg-slate-500 hover:bg-slate-700 text-white'>-</button>
                   <input value="1" type='number' readOnly className=' text-center p-2 border-none w-10' />
                   <button className='px-4 text-2xl border bg-slate-500 hover:bg-slate-700 text-white'>+</button>
-                </div> {''}
-                <button className='btn-primary capitalize'>Add To Cart</button>
+                </div> {''} */}
+                <button className='btn btn-primary btn-wide text-white capitalize'>Add To Cart</button>
               </div>
               <p>Status: {" "}
                 <b className={product.Stock < 1 ? "text-red-700" : "text-green-700"}>{product.Stock < 1 ? "OutOfStock" : "InStock"}</b>
@@ -147,12 +150,12 @@ const options = {
               <div className="pd-desc my-4 flex flex-col justify-start items-start border-t-2 ">
                 <h2 className='text-center pt-2 text-xl mb-4'> Description</h2>
                 <p className=' font-normal w-2/3 mb-10'>{product.description}</p>
-                <button className='btn-primary capitalize mb-20 '>Submit Review</button>
+                {/* <button className='btn-primary capitalize mb-20 '>Submit Review</button> */}
               </div>
             </div>
           </div>
         </div>
-        <div className="pd-bottom py-10 flex flex-col justify-center items-center border-t-2 ">
+        {/* <div className="pd-bottom py-10 flex flex-col justify-center items-center border-t-2 ">
           <div className="flex items-center justify-center">
             <h2 className=" mb-4 p-4 w-fit border-b-2 self-center">REVIEWS</h2>
           </div>
@@ -167,8 +170,8 @@ const options = {
 
               <p className='no-review text-center'>No Reviews Yet</p>
 
-          }
-        </div>
+          } 
+        </div> */}
       </div>
     </Fragment>
   )
