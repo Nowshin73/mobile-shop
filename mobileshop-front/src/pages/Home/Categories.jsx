4b9c719abe82bf768d883aefb37d60ec47122c47
Nowshin73 <68994761+Nowshin73@users.noreply.github.com>
2024-12-25 00:20:25 +0600

    category added

--- mobileshop-front/src/pages/Home/Categories.jsx
+++ mobileshop-front/src/pages/Home/Categories.jsx
@@ -1,34 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Categories = () => {
-  return (
-    <div className="bg-gray-100">
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-        <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+const Categories = ({ category }) => {
 
-        <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-          {callouts.map((callout) => (
-            <div key={callout.name} className="group relative">
-              <img
-                alt={callout.imageAlt}
-                src={callout.imageSrc}
-                className="w-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-[2/1] lg:aspect-square"
-              />
-              <h3 className="mt-6 text-sm text-gray-500">
-                <a href={callout.href}>
-                  <span className="absolute inset-0" />
-                  {callout.name}
-                </a>
-              </h3>
-              <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+    return (
+        <div key={category.name} className="card  image-full w-96 ">
+
+            <figure>
+                <img
+                    alt={category.name}
+                    src={category.image} />
+            </figure>
+            <div className="card-body flex flex-col justify-center items-center">
+                <h2 className="card-title text-white"> {category.name}</h2>
+
+                <div className="card-actions justify-end">
+                    <Link to="/products"><button className="btn btn-primary">Buy Now</button></Link>
+                </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  </div>
-  )
+              
+    )
 }
 
 export default Categories
--- mobileshop-front/src/pages/Home/Home.jsx
+++ mobileshop-front/src/pages/Home/Home.jsx
@@ -6,15 +6,28 @@ import Swal from 'sweetalert2'
 
 import Product from "../../pages/Products/Product";
 import Banner from "../../components/banner/Banner";
+import Categories from "./Categories";
 const Home = () => {
-const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-useEffect(() => {
-      fetch('http://localhost:5000/products')
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
       .then(res => res.json())
       .then(data => setProducts(data));
-},[])
+  }, [])
 
+  const categories = [{
+    name: "Android",
+    image:"https://i.ibb.co.com/5xGBtZn/image.png"
+  },
+  {
+    name: "iPhone",
+    image:"https://i.ibb.co.com/BtYPMC0/image.png"
+  },
+  {
+    name: "CellPhone",
+    image:"https://i.ibb.co.com/fvXnq4y/image.png"
+  }];
 
   return (
     <Fragment>
@@ -30,6 +43,18 @@ useEffect(() => {
               <Product key={product._id} product={product} />
             ))}
           </div>
+
+        </div>
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <div className="flex items-center justify-center">
+            <h2 className=" mb-4 p-4 w-fit border-b-2 self-center card-title">Categories</h2>
+          </div>
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {categories.map((category) => (
+               <Categories key={category.name} category={category}></Categories>
+            ))}
+          </div>
+         
 
         </div>
       </div>
